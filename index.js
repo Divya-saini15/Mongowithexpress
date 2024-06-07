31fa09c0bcecb0ccd7d998395dbf45d429001295
divya-sarvika <divya.saini@sarvika.com>
2024-06-07 16:05:35 +0530

    create post and new page api

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app=express();
 const mongoose = require("mongoose");
 const path = require("path");
-const Chat =require("./models/chat.js")
+const Chat =require("./models/chat.js");
+const { constrainedMemory } = require("process");
 
 app.set("views",path.join(__dirname,"views"));
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main().then(()=>{
     console.log("connection is successful");
@@ -15,6 +18,39 @@ main().then(()=>{
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/chatdb');
 }
+
+//List record
+app.get("/chats", async (req,res)=>{
+    const chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+})
+
+//New Route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+//Create Route
+app.post("/new",(req,res)=>{
+    const {from , to , msg} = req.body;
+    const newChat = new Chat({
+        from:from,
+        to:to,
+        msg:msg,
+        created_at :new Date(),
+    });
+    newChat
+    .save()
+    .then((res)=>{
+        console.log("chat was saved");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+    res.redirect("/chats");
+})
+
 
 
 app.get("/",(req,res) => {
